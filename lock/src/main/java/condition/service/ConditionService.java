0c218b858316f9bb423b691f79bcd9b564c196fd
why20104712 <why20104712@gmail.com>
2017-06-02 22:32:36 +0800

    Condition使用

--- lock/src/main/java/condition/service/ConditionService.java
+++ lock/src/main/java/condition/service/ConditionService.java
@@ -22,8 +22,11 @@ public class ConditionService {
 
         try {
             reentrantLock.lock();
-            System.out.println("当前线程名称：" + Thread.currentThread().getName() + " 当前时间：" + System.currentTimeMillis());
+            System.out.println("begin await() ThreadName="
+                    + Thread.currentThread().getName());
             condition.await();
+            System.out.println("  end await() ThreadName="
+                    + Thread.currentThread().getName());
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
@@ -40,14 +43,25 @@ public class ConditionService {
 
         try {
             reentrantLock.lock();
-            System.out.println("当前线程名称：" + Thread.currentThread().getName() + " 当前时间：" + System.currentTimeMillis());
+            System.out.println("begin signal() ThreadName="
+                    + Thread.currentThread().getName() + " time="
+                    + System.currentTimeMillis());
             condition.signal();
+            Thread.sleep(5000);
+            System.out.println("  end signal() ThreadName="
+                    + Thread.currentThread().getName() + " time="
+                    + System.currentTimeMillis());
+            System.out.println("signal执行完不释放锁，执行完同步方法才释放锁");
+
+
+
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         } finally {
             reentrantLock.unlock();
             System.out.println("当前线程名称：" + Thread.currentThread().getName() + " 锁释放了");
         }
-
-
     }
 
 }
